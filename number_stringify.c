7185f839afe7cf2336585ee9a9aa8cf304925f60
kilemile26 <harunakilemile@gmail.com>
2023-04-25 16:38:37 -0700

    main.h

---
+++ number_stringify.c
@@ -0,0 +1,63 @@
+#include "main.h"
+/**
+ * interpret_spec - Interpret format specifier string, creates formatted string
+ * Return: A pointer to the formatted string.
+ */
+
+/* strlen("18446744073709551615") = 20 */
+#define MAX_INT_LEN 20
+
+char *interpret_spec(char *fmt_spec, strQ *out_string)
+{
+	/* TO DO */
+}
+
+/**
+ * double_to_str - Converts double-precision floating-point number to a string
+ * @num: The double-precision floating-point number to convert
+ * @custom: Pointer to a buffer to hold the resulting string
+ * Return: A pointer to the resulting string.
+ */
+
+char *double_to_str(double num, strQ *custom)
+{
+	/* TO DO */
+}
+
+/**
+ * int_to_str - Converts integer to string representation in specified radix
+ * @num: The integer to convert
+ * @radix: The radix to use for the conversion
+ * @custom: Pointer to a buffer to hold the resulting string
+ * Return: A pointer to the resulting string.
+ */
+
+char *int_to_str(int num, size_t radix, strQ *custom)
+{
+	int i;
+	uint64_t n, base = 1;
+	char *str;
+
+	if (num == INT32_MIN)
+	{
+		str = malloc(11);
+		return (strcpy(str, "-2147483648"));
+	}
+	if (radix == 0)
+	{
+		str = malloc(4);
+		return (strcpy(str, "NaN"));
+	}
+	str = malloc(MAX_INT_LEN + 1 + 1);
+	VALID_ALLOC(str);
+
+	str[MAX_INT_LEN + 1] = '\0';
+	for (i = MAX_INT_LEN; i > 0; i--)
+	{
+		n = (num / base) % radix;
+		str[i] = (n < 10) ? ('0' + n) : ('A' + n - 10);
+		base *= radix;
+	}
+	strip(str + 1);
+	return (str);
+}
